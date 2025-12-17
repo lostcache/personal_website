1d84c59e7cd5b1714c9e06beabbed900d7bd30a8
Harsh Devisha <harshdevisha9111@gmail.com>
2025-12-16 20:06:29 -0800

    adds zue blog posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,13 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
+import { remarkReadingTime } from './remark-reading-time.mjs';
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://lostcache.github.io",
   base: "/personal_website",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx({
+    remarkPlugins: [remarkReadingTime],
+  }), sitemap()],
 });
