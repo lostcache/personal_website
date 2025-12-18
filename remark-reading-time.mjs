db6c2e003b8dc7e89879782a6975c4c565df2152
Harsh Devisha <harshdevisha9111@gmail.com>
2025-12-18 14:18:10 -0800

    prettier

--- remark-reading-time.mjs
+++ remark-reading-time.mjs
@@ -1,13 +1,12 @@
-
-import getReadingTime from 'reading-time';
-import { toString } from 'mdast-util-to-string';
+import getReadingTime from "reading-time";
+import { toString } from "mdast-util-to-string";
 
 export function remarkReadingTime() {
-	return function (tree, { data }) {
-		const textOnPage = toString(tree);
-		const readingTime = getReadingTime(textOnPage);
-		// readingTime.text will give us something like "3 min read"
-        // readingTime.minutes gives us the number
-		data.astro.frontmatter.readingTime = readingTime.text;
-	};
+  return function (tree, { data }) {
+    const textOnPage = toString(tree);
+    const readingTime = getReadingTime(textOnPage);
+    // readingTime.text will give us something like "3 min read"
+    // readingTime.minutes gives us the number
+    data.astro.frontmatter.readingTime = readingTime.text;
+  };
 }
--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Harsh Devisha";
-export const SITE_DESCRIPTION = "A blog about distributed systems, systems programming, and full-stack development by Harsh Devisha.";
+export const SITE_DESCRIPTION =
+  "A blog about distributed systems, systems programming, and full-stack development by Harsh Devisha.";
